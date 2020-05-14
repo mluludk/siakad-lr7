8c32e78f831c0640bc3035d5086fc791caf28d40
amuadib <a.muadib@gmail.com>
2020-05-14 20:40:05 +0700

    Tidyng

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,104 +1,78 @@
 <?php
-	
-	namespace Siakad\Http\Controllers;
-	
-	use Illuminate\Http\Request;
-	
-	
-	use Siakad\Mail;
-	
-	//use Siakad\Http\Requests;
-	use Siakad\Http\Controllers\Controller;
-	
-	class MailController extends Controller
-	{
-		/**
-			* Display a listing of the resource.
-			*
-			* @return \Illuminate\Http\Response
-		*/
-		public function index()
-		{
-			$user = \Auth::user();
-			$my_mail = Mail::with('from.authable') -> where('recipient', $user -> id);
-			
-			$inbox = $my_mail -> orderBy('time', 'desc') -> paginate(30);
-			// $unread = $my_mail -> where('read', 'n') -> count();
-			
-			return view('mail.index', compact('inbox'));
-		}
-		
-		/**
-			* Show the form for creating a new resource.
-			*
-			* @return \Illuminate\Http\Response
-		*/
-		public function create()
-		{
-			return view('mail.create');
-		}
-		
-		/**
-			* Store a newly created resource in storage.
-			*
-			* @param  \Illuminate\Http\Request  $request
-			* @return \Illuminate\Http\Response
-		*/
-		public function store(Request $request)
-		{
-			//
-		}
-		
-		/**
-			* Display the specified resource.
-			*
-			* @param  int  $id
-			* @return \Illuminate\Http\Response
-		*/
-		public function show($title, $id)
-		{
-			$user = \Auth::user();
-			$my_mail = Mail::with('from.authable') -> where('recipient', $user -> id);
-			// $unread = $my_mail -> where('read', 'n') -> count();
-			
-			$mail = Mail::with('from.authable') -> find($id);
-			$mail -> update(['read' => 'y']);
-			
-			return view('mail.show', compact('mail'));
-		}
-		
-		/**
-			* Show the form for editing the specified resource.
-			*
-			* @param  int  $id
-			* @return \Illuminate\Http\Response
-		*/
-		public function edit($id)
-		{
-			//
-		}
-		
-		/**
-			* Update the specified resource in storage.
-			*
-			* @param  \Illuminate\Http\Request  $request
-			* @param  int  $id
-			* @return \Illuminate\Http\Response
-		*/
-	public function update(Request $request, $id)
-	{
-	//
-	}
-	
+
+namespace Siakad\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+use Siakad\Mail;
+
+use Siakad\Http\Controllers\Controller;
+
+class MailController extends Controller
+{
 	/**
-	* Remove the specified resource from storage.
-	*
-	* @param  int  $id
-	* @return \Illuminate\Http\Response
-	*/
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$user = \Auth::user();
+		$my_mail = Mail::with('from.authable')->where('recipient', $user->id);
+
+		$inbox = $my_mail->orderBy('time', 'desc')->paginate(30);
+
+		return view('mail.index', compact('inbox'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('mail.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($title, $id)
+	{
+		$user = \Auth::user();
+		$my_mail = Mail::with('from.authable')->where('recipient', $user->id);
+
+		$mail = Mail::with('from.authable')->find($id);
+		$mail->update(['read' => 'y']);
+
+		return view('mail.show', compact('mail'));
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
 	public function destroy($id)
 	{
-	//
+		//
 	}
-	}
-		
+}
